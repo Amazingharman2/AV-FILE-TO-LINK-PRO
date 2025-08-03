e0ab33826e9dfccce79125d532df56c8fdead941
Amazingharman2 <chpudas@gmail.com>
2025-08-03 11:00:57 +0530

    Update info.py
    Signed-off-by: Amazingharman2 <chpudas@gmail.com>

--- info.py
+++ info.py
@@ -85,8 +85,8 @@ else:
 PORT = int(getenv('PORT', '2626'))  # Port for web server
 NO_PORT = str(getenv("NO_PORT", False)).lower() in ("true", "1", "yes")  # Disable port in URL
 HAS_SSL = str(getenv("HAS_SSL", False)).lower() in ("true", "1", "yes")  # Use HTTPS if True
-BIND_ADDRESS = getenv("WEB_SERVER_BIND_ADDRESS", "https://av-file-to-link-pro-50gn.onrender.com")  # Server bind address
+BIND_ADDRESS = getenv("WEB_SERVER_BIND_ADDRESS", "av-file-to-link-pro-q9b6.onrender.com")  # Server bind address
 FQDN = getenv("FQDN", "") or BIND_ADDRESS  # Full domain name or fallback to bind address
-PORT_SEGMENT = "" if NO_PORT else f":{PORT}"  # Port in URL if not disabled
+PORT_SEGMENT = "/"  # Port in URL if not disabled
 PROTOCOL = "https" if HAS_SSL else "http"  # Protocol for URL
 URL = f"{PROTOCOL}://{FQDN}{PORT_SEGMENT}"  # Final generated base URL
